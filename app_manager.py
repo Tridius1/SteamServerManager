8a6865003a8d3b7c14c32fb1aaeee21bd02cf472
Tridius1 <tristan.t73@gmail.com>
2022-12-12 14:50:11 -0700

    Minor fixes and improvements

--- app_manager.py
+++ app_manager.py
@@ -1,10 +1,12 @@
 import os
+from sys import exit
 import shutil
 import subprocess
 import pickle
 import re
 import datetime
 from getpass import getpass
+from getpass import getuser
 
 try:
     from simple_term_menu import TerminalMenu
@@ -41,10 +43,16 @@ def choose_validate(prompt, true_regex, false_regex):
 
 class app_manifest:
     def __init__(self):
+
+        if not getuser() == 'steam':
+            print("Error: Must be user 'steam' to manage SteamCMD applications. You are {}".format(getuser()))
+            exit(-1)
+
         self.data = None
         self.filename = "app_manifest"
 
         self.valid = self.load()
+        self.modified = False
 
     # opens the manifest and unpickles it
     # returns sucsess
@@ -65,30 +73,46 @@ class app_manifest:
         outf = open(self.filename, 'wb')
         pickle.dump(self.data, outf)
         outf.close()
+        self.modified = False
+        return True
 
-    def newApp(self, name, app_id, anon):
+    def newApp(self, name, app_id, anon, args):
         if os.path.exists('/home/steam/' + name + '/'):
             if not confirm("Existing directory '"+name+"' found. Continue anyway?"):
                 #cancel
                 return
         else:
             os.mkdir('/home/steam/' + name + '/')
-        self.data[name] = {"id":app_id, "anon":anon, "last_update":None}
+        if args is None:
+            args = []
+        self.data[name] = {"id":app_id, "anon":anon, "args":args}
+
+        self.modified = True
         return
 
     def createApp(self):
-        print("Creating a new sever/n")
+        print("Creating a new sever\n")
         
         name = ask_validate("Name server: ", "^[^*&%\s]+$")
-        app_id = ask_validate("Enter app id: " , "^[0-9]{2,6}$")
-        anon = choose_validate("Is user verification required? [y/n]", "^[yY]$|^[yY][eE][sS]$", "^[nN]$|^[nN][oO]$")
+        app_id = ask_validate("Enter app id: " , "^[0-9]{2,12}$")
+        anon_menu = TerminalMenu(['No', 'Yes'], title="Is user verification required?")
+        anon = anon_menu.show()
 
-        self.newApp(str(name), int(app_id), bool(anon))
+        args_menu = TerminalMenu(['No', 'Yes'], title="Additional Arguments?")
+        args = None
+        if args_menu.show():
+            args = ask_validate("Enter arguments seperated by spaces: ", "^.+$").split()
+            # enter args
+        
+
+        self.newApp(str(name), int(app_id), bool(anon), args)
+        self.modified = True
         return
 
     def removeApp(self, name):
         del self.data[name]
         print("Server '{}' removed".format(name))
+        self.modified = True
         return
         
     #util
@@ -98,13 +122,14 @@ class app_manifest:
             print("    No existing servers found  \n")
             return
 
-        row_template = "{:>10} " * 3
+        row_template = "{:>12} " * 4
         print("\nServers:\n")
-        print(row_template.format("Name", "App ID", "Anonymous"))
+        print(row_template.format("Name", "App ID", "Login", "Arguments"))
         for name, row in self.data.items():
-            print(row_template.format(name, *row.values()))
+            print(row_template.format(name, *map(str, row.values())))
         print('\n')
         return
+
 
 
     # Updaters
@@ -112,20 +137,41 @@ class app_manifest:
         if not self.data[name]["anon"]:
             self.update_anon(name)
         else:
-            print("help")
+            self.update_login(name)
 
     def update_anon(self, name):
-        command = ["./steamcmd/steamcmd.sh", "+login", "anonymous", "+force_install_dir", "/home/steam/"+name+'/', "+app_update", str(self.data[name]["id"]), "validate", "+quit"]
-        print(command)
+        appID = self.data[name]["id"]
+        command = ["./steamcmd/steamcmd.sh", "+force_install_dir", "/home/steam/"+name+'/', "+login", "anonymous", "+app_update", str(appID), "validate", "+quit"]
+        #Apply user defined arguments
+        pos = 7
+        for arg in self.data[name]["args"]:
+            command.insert(pos, arg)
+            pos += 1
+        #print(command)
+        print("Anonymously updating app {} : '{}'".format(appID, name))
         steamProc = subprocess.Popen(command, stdout=subprocess.PIPE, bufsize=1)
-        for line in iter(steamProc.stdout.readline, b''):
-            print(str(line))
+        steamOut(steamProc)
         steamProc.wait()
 
+    def update_login(self, name):
+        appID = self.data[name]["id"]
 
-        #stdout, stderr = steamProc.communicate()
-        #print("{}".format(steamProc.stdout))
+        usrname = input("Enter Steam username: ")
+        pswd = getpass("Enter Steam password: ")
+
+        command = ["./steamcmd/steamcmd.sh", "+login", usrname, pswd, "+force_install_dir", "/home/steam/"+name+'/', "+app_update", str(appID), "validate", "+quit"]
+        #print(command)
+        print("Logging in and updating app {} : '{}'".format(appID, name))
+        steamProc = subprocess.Popen(command, stdout=subprocess.PIPE, bufsize=1)
+        steamOut(steamProc)
+        steamProc.wait()
     
+
+def steamOut(steamProc):
+    usr = getuser()
+    for line in iter(steamProc.stdout.readline, b''):
+        nline = str(line)[2:-3].strip()
+        print("[{}][steamCMD]  {}".format(usr, nline))
 
 
 def confirm(prompt):
@@ -156,7 +202,7 @@ if __name__ == "__main__":
     while not quit:
         Manifest.printData()
 
-        main_menu = TerminalMenu(["Update a server", "Update all servers", "New server", "Remove server", "Exit"], title="Main Menu")
+        main_menu = TerminalMenu(["Update a server", "New server", "Remove server", "Exit"], title="Main Menu")
         selection = main_menu.show()
         if selection == 0:
             # Pick server
@@ -166,12 +212,10 @@ if __name__ == "__main__":
             server = servers[server]
             if confirm("Update server " + server + "?"):
                 Manifest.update(server)
+        
         elif selection == 1:
-            pass
-
-        elif selection == 2:
             Manifest.createApp()
-        elif selection == 3:
+        elif selection == 2:
             #remove server
             servers = list(Manifest.data.keys())
             server_menu = TerminalMenu(servers, title="Select server to remove")
